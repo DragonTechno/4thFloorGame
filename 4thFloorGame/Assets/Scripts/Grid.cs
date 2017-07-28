622d80205c25e65d7d7005e389196e5111cdbb66
DragonTechno <12izzy3@gmail.com>
2017-07-27 17:37:53 -0700

    Fixed Grid System
    Easy peasy, make levels easy.

---
+++ 4thFloorGame/Assets/Scripts/Grid.cs
@@ -0,0 +1,178 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+
+	public bool displayGridGizmos;
+	public Vector2 gridWorldSize;
+	public float nodeRadius;
+	public TerrainType[] walkableRegions;
+	public float rayAdjust;
+	LayerMask walkableMask;
+	Node[,] grid;
+	Dictionary<int,int> walkableDict = new Dictionary<int, int>();
+
+	float nodeDiameter;
+	int gridSizeX, gridSizeY;
+
+	void Awake()
+	{
+		nodeDiameter = nodeRadius * 2;
+		gridSizeX = Mathf.RoundToInt(gridWorldSize.x/nodeDiameter);
+		gridSizeY = Mathf.RoundToInt(gridWorldSize.y/nodeDiameter);
+		foreach (TerrainType region in walkableRegions)
+		{
+			walkableMask.value |= region.terrainMask.value;
+			walkableDict.Add ((int)Mathf.Log(region.terrainMask.value,2),region.terrainPenalty);
+		}
+		CreateGrid ();
+	}
+
+	public int MaxSize
+	{
+		get {
+			return gridSizeX * gridSizeY;
+		}	
+	}
+
+	void CreateGrid()
+	{
+		grid = new Node[gridSizeX, gridSizeY];
+		Vector3 worldBottomLeft = transform.position - Vector3.right * (gridWorldSize.x / 2 - rayAdjust) - Vector3.up * gridWorldSize.y / 2;
+		int movementPenalty = 0;
+
+		for (int x = 0; x < gridSizeX; x++)
+		{
+			Vector3 startPoint = worldBottomLeft + Vector3.right * x * nodeRadius * 2;
+			RaycastHit2D[] column = Physics2D.RaycastAll (startPoint, Vector3.up, gridSizeY * nodeRadius * 2, walkableMask);
+			foreach(RaycastHit2D hit in column)
+			{
+				bool setGrid = false;
+				GameObject other = hit.collider.gameObject;
+				int[] gridPos = GridPosFromWorldPoint(hit.transform.position+Vector3.up*.5f);
+				int gridY = gridPos [1];
+				if (other.tag == "Ground")
+				{
+					gridY += 1;
+				}
+				if (grid [x, gridY] == null)
+				{
+					setGrid = true;
+				}
+				else if(grid[x,gridY].tag == "Climbable")
+				{
+					setGrid = true;
+				}
+				if (setGrid)
+				{
+					walkableDict.TryGetValue (hit.collider.gameObject.layer, out movementPenalty);
+					Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.up * (gridY * nodeDiameter + nodeRadius);
+					grid[x, gridY] = new Node (true, worldPoint,x,gridY,movementPenalty,other.tag);
+				}
+			}
+			for (int y = 0; y < gridSizeY; y++)
+			{
+				if (grid [x, y] == null)
+				{
+					Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.up * (y * nodeDiameter + nodeRadius);
+					grid[x, y] = new Node (false, worldPoint,x,y,0,"");
+				}
+			}
+		}
+		/*for (int x = 0; x < gridSizeX; x++)
+		{
+			for (int y = 0; y < gridSizeY; y++)
+			{
+				Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.up * (y * nodeDiameter + nodeRadius);	
+
+				int movementPenalty = 0;
+
+				if (walkable)
+				{
+					Ray ray = new Ray (worldPoint + Vector3.forward * 50, Vector3.back);
+					RaycastHit hit;
+					if(Physics.Raycast(ray,out hit, 100, walkableMask))
+					{
+						walkableDict.TryGetValue (hit.collider.gameObject.layer, out movementPenalty);
+					}
+				}
+
+				grid[x, y] = new Node (walkable, worldPoint,x,y,movementPenalty);
+			}
+		}*/
+	}
+
+	public List<Node> GetNeighbors(Node node)
+	{
+		List<Node> neighbors = new List<Node> ();
+		for (int x = -1; x <= 1; x++)
+		{
+			for (int y = -1; y <= 1; y++)
+			{
+				if (x == y || x == -y)
+				{
+					continue;
+				}
+				int checkX = node.gridX + x;
+				int checkY = node.gridY + y;
+				if (checkY >= 0 && checkX >= 0 && checkY < gridSizeY && checkX < gridSizeX)
+				{
+					neighbors.Add (grid [checkX, checkY]);
+				}
+			}
+		}
+
+		return neighbors;
+	}
+
+	public Node NodeFromWorldPoint(Vector3 worldPosition)
+	{
+		int[] coord= GridPosFromWorldPoint(worldPosition);
+		return grid [coord[0], coord[1]];
+	}
+
+	public int[] GridPosFromWorldPoint (Vector3 worldPosition)
+	{
+		float percentX = (worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x;
+		float percentY = (worldPosition.y + gridWorldSize.y / 2) / gridWorldSize.y;
+		percentX = Mathf.Clamp01 (percentX);
+		percentY = Mathf.Clamp01 (percentY);
+
+		int x = Mathf.RoundToInt((gridSizeX - 1) * percentX);
+		int y = Mathf.RoundToInt((gridSizeY - 1) * percentY*1.05f-.5f);
+		int[] coord = {x,y};
+		return coord;
+	}
+		
+	void OnDrawGizmos() {
+		Gizmos.DrawWireCube(transform.position,new Vector3(gridWorldSize.x,gridWorldSize.y,1));
+
+		if (grid != null && displayGridGizmos)
+		{
+			foreach(Node n in grid)
+			{	
+				if (n.tag == "Climbable")
+				{
+					Gizmos.color = Color.blue;
+				}
+				else if (n.tag == "Ground")
+				{
+					Gizmos.color = Color.white;
+				}
+				else
+				{
+					Gizmos.color = Color.clear;
+				}	
+				Gizmos.DrawCube (n.WorldPosition, Vector3.one * (nodeDiameter-.1f));
+			}
+		}
+	}
+
+	[System.Serializable]
+	public class TerrainType
+	{
+		public LayerMask terrainMask;
+		public int terrainPenalty;
+	}
+}
---
+++ 4thFloorGame/Assets/Scripts/UnitMov.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitMov : MonoBehaviour {
+
+	const float minPathUpdateTime = .2f;
+	const float updateMoveThresh = .5f;
+	//public Transform target;
+	public Vector3 targetPos;
+	public bool newTarget;
+	public float checkTime = 0;
+	public float speed = 1;
+	Vector3[] path;
+	int targetIndex;
+
+	// Use this for initialization
+	void Start () {
+		targetPos = transform.position;
+		PathRequestManager.RequestPath (new PathRequest (transform.position, targetPos, OnPathFound));
+	}
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown (0))
+		{
+			newTarget = true;
+			Vector3 mouse = Input.mousePosition;
+			mouse.z = 50;
+			print (mouse);
+			mouse = Camera.main.ScreenToWorldPoint (mouse);
+			print (mouse);
+			targetPos = mouse;
+			PathRequestManager.RequestPath (new PathRequest (transform.position, targetPos, OnPathFound));
+		}
+	}
+
+	public void OnPathFound(Vector3[] newPath, bool pathSuccessful)
+	{
+		if (pathSuccessful)
+		{
+			path = newPath;
+			StopCoroutine("FollowPath");
+			StartCoroutine ("FollowPath");
+		}
+	}
+
+	/*IEnumerator UpdatePath()
+	{
+		if (Time.timeSinceLevelLoad < .3f)
+		{
+			yield return new WaitForSeconds (.3f);
+		}
+		PathRequestManager.RequestPath (new PathRequest (transform.position, targetPos, OnPathFound));
+
+		while (true)
+		{
+			yield return new WaitForSeconds (minPathUpdateTime);
+			if (newTarget)
+			{
+				PathRequestManager.RequestPath (new PathRequest (transform.position, targetPos, OnPathFound));
+			}
+		}
+
+	}*/
+
+	IEnumerator FollowPath()
+	{
+		targetIndex = 0;
+		Vector3 currentWaypoint = path [0];
+
+		while (true)
+		{
+			if (transform.position == currentWaypoint)
+			{
+				print ("Waypoint reached: " + transform.position + ", " + currentWaypoint);
+				targetIndex++;
+				if (targetIndex >= path.Length)
+				{
+					newTarget = false;
+					yield break;
+				}
+				currentWaypoint = path [targetIndex];
+			}
+
+			transform.position = Vector3.MoveTowards (transform.position, currentWaypoint, speed*Time.deltaTime);
+			yield return null;
+		}
+	}
+
+	public void OnDrawGizmos()
+	{
+		if (path != null)
+		{
+			for (int i = targetIndex; i < path.Length; i++)
+			{
+				Gizmos.color = Color.magenta;
+				Gizmos.DrawCube (path [i], Vector3.one);
+
+				if (i == targetIndex)
+				{
+					Gizmos.DrawLine (transform.position, path [i]);
+				}
+				else
+				{
+					Gizmos.DrawLine (path [i - 1], path [i]);
+				}
+			}
+		}
+	}
+}
